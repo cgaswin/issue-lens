cae2f30a80b45554c3365a91b5c5fd0d07550675
cgaswin <cgaswin333@gmail.com>
2026-01-26 23:02:34 +0530

    fix: bump firefox min version to 128.0 to support data_collection_permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,6 +9,19 @@ export default defineConfig({
     name: 'Issue Lens',
     permissions: ['storage'],
     host_permissions: ['https://api.github.com/*'],
+    browser_specific_settings: {
+      gecko: {
+        id: '{da070c02-e25f-4d92-911e-0899882294e0}',
+        strict_min_version: '128.0',
+        // @ts-ignore - WXT types might not have this new field yet
+        data_collection_permissions: {
+          ad_interaction: false,
+          search_query: false,
+          tab_history: false,
+          web_history: false
+        }
+      }
+    }
   },
   vite: () => ({
     plugins: [tailwindcss()],
